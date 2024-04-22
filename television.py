1d4fec3e43fbae6a90e508319c2dfb46231bac5f
LizCullison <blustarznight@gmail.com>
2024-04-21 20:34:34 -0500

    Fourth file

--- television.py
+++ television.py
@@ -7,59 +7,59 @@ class Television:
     
     def __init__(self) -> None:
         # Instance variables
-        self._status = False  # Television is initially off
-        self._muted = False  # Television is initially not muted
-        self._volume = self.MIN_VOLUME  # Initial volume is set to minimum
-        self._channel = self.MIN_CHANNEL  # Initial channel is set to minimum
+        self.__status = False  # Television is initially off
+        self.__muted = False  # Television is initially not muted
+        self.__volume = self.MIN_VOLUME  # Initial volume is set to minimum
+        self.__channel = self.MIN_CHANNEL  # Initial channel is set to minimum
     
     def power(self) -> None:
         # Toggle power status
-        self._status = not self._status
+        self.__status = not self.__status
     
     def mute(self) -> None:
         # Toggle mute status if TV is on
-        if self._status:
-            if not self._muted:
+        if self.__status:
+            if not self.__muted:
                 # Save current volume and set volume to 0 when muting
-                self._previous_volume = self._volume  # Save the current volume
-                self._muted = True
-                self._volume = self.MIN_VOLUME  # Set volume to zero
+                self.__previous_volume = self.__volume  # Save the current volume
+                self.__muted = True
+                self.__volume = self.MIN_VOLUME  # Set volume to zero
             else:
                 # Restore previous volume when unmuting
-                self._muted = False
-                self._volume = self._previous_volume  # Restore the saved volume
+                self.__muted = False
+                self.__volume = self.__previous_volume  # Restore the saved volume
         else:
-            self._muted = False
+            self.__muted = False
     
     def channel_up(self) -> None:
         # Increase channel if TV is on
-        if self._status:
-            self._channel = (self._channel + 1) % (Television.MAX_CHANNEL + 1)
+        if self.__status:
+            self.__channel = (self.__channel + 1) % (Television.MAX_CHANNEL + 1)
     
     def channel_down(self) -> None:
         # Decrease channel if TV is on
-        if self._status:
-            self._channel = (self._channel - 1) % (Television.MAX_CHANNEL + 1)
+        if self.__status:
+            self.__channel = (self.__channel - 1) % (Television.MAX_CHANNEL + 1)
     
     def volume_up(self) -> None:
         # Increase volume if TV is on and unmute TV if muted
-        if self._status:
-            if self._muted:
-                self._muted = False
-                self._volume = self._previous_volume
-            if self._volume < Television.MAX_VOLUME:
-                self._volume += 1
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+                self.__volume = self.__previous_volume
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
     
     def volume_down(self) -> None:
         # Decrease volume if TV is on and unmute TV if muted
-        if self._status:
-            if self._muted:
-                self._muted = False
-                self._volume = self._previous_volume
-            if self._volume > Television.MIN_VOLUME:
-                self._volume -= 1
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+                self.__volume = self.__previous_volume
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
     
     def __str__(self) -> str:
         # Return the TV status, channel, and volume as a formatted string
-        return f"Power = {self._status}, Channel = {self._channel}, Volume = {self._volume}"
+        return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
 
